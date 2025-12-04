31dfe93c6ea7788633bd7e78cbbc06175f58e696
onlyyourza <paseoking456@gmail.com>
2025-12-04 20:40:45 +0700

    1.	Create a new class named CubeStudentIDNumber that has function to calculate area and volume!

---
+++ Cube14.java
@@ -0,0 +1,11 @@
+public class Cube14 {
+
+    public static int calculateArea(int length, int width) {
+        return length * width;
+    }
+
+    public static int calculateVolume(int length, int width, int height) {
+        return length * width * height;
+    }
+
+}
