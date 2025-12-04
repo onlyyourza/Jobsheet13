c66d7d98d0f0c6511689ae35c9c737b22f239c7d
onlyyourza <paseoking456@gmail.com>
2025-12-04 19:57:34 +0700

    Experiment 4: Calling Function from the Other Function

---
+++ ExpressingGratitude_14.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;;
+public class ExpressingGratitude_14 {
+    public static String getGreetingRecipient() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input the name of the people you want to greet : ");
+        String recipientName = input.nextLine();
+        input.close();
+        return recipientName;
+    }
+
+    public static void sayThankYou() {
+        String name = getGreetingRecipient();
+        System.out.println("Thankyou "+name+" for being the best teacher in the world\n" +
+            " you inspired in me a love for learning and made me feel like i could ask you anything."
+        );
+    }
+
+    public static void main(String[] args) {
+        sayThankYou();
+    }
+}
